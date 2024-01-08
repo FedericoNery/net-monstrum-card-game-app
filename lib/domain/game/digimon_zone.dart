7e24eca80df6b5ac931e7c69184b60811f04e526
Federico Nery <fedegnery@gmail.com>
2024-01-08 14:12:11 -0300

    nmcg#19 :: Implementar sistema de evolución al tener 3 o más cartas del mismo digimon

--- lib/domain/game/digimon_zone.dart
+++ lib/domain/game/digimon_zone.dart
@@ -19,8 +19,39 @@ class DigimonZone {
         attackPoints += card.attackPoints;
     }
 
-    return attackPoints;
+    int evolutionAttackPoints = getEvolutionAttackPoints();
+
+    return attackPoints + evolutionAttackPoints;
   }
+
+  Map<String, int> getCardCounts(){
+    Map<String, int> cardCounts = {};
+    for (Card card in cards) {
+      if (cardCounts.containsKey(card.digimonName)) {
+        cardCounts[card.digimonName] = cardCounts[card.digimonName]! + 1;
+      } else {
+        cardCounts[card.digimonName] = 1;
+      }
+    }
+
+    return cardCounts;
+  }
+
+  int getEvolutionAttackPoints() {
+    Map<String, int> cardCounts = getCardCounts();
+
+    int totalAttackPoints = 0;
+    for (String digimonName in cardCounts.keys) {
+      int count = cardCounts[digimonName]!;
+      if (count >= 3) {
+        Card card = cards.firstWhere((card) => card.digimonName == digimonName);
+        totalAttackPoints += card.attackPoints * count;
+      }
+    }
+    return totalAttackPoints;
+  }
+
+
 
   int getHealthPoints(){
     int healthPoints = 0;
@@ -28,7 +59,23 @@ class DigimonZone {
       healthPoints += card.healthPoints;
     }
 
-    return healthPoints;
+    int evolutionHealthPoints = getEvolutionHealthPoints();
+
+    return healthPoints + evolutionHealthPoints;
+  }
+
+  int getEvolutionHealthPoints() {
+    Map<String, int> cardCounts = getCardCounts();
+
+    int totalHealthPoints = 0;
+    for (String digimonName in cardCounts.keys) {
+      int count = cardCounts[digimonName]!;
+      if (count >= 3) {
+        Card card = cards.firstWhere((card) => card.digimonName == digimonName);
+        totalHealthPoints += card.healthPoints * count;
+      }
+    }
+    return totalHealthPoints;
   }
 
   //TODO :: si hay 3 o mas cartas iguales, entonces sumar la totalidad de cada carta y mostrar la imagen de la evolución
